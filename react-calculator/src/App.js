dbd2005558262c00393f1856580b27a5c7678344
cspdevs000 <cspdevs000@gmail.com>
2022-03-30 18:17:53 -0400

    calculator styled

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,7 +1,32 @@
+import './styles.css';
+
 function App() {
   return (
-    <div className="calcGrid">
-      HI
+    <div className="calc-grid">
+      <div className="output">
+        <div className="prev-operand">
+        </div>
+        <div className="current-operand">
+        </div>
+      </div>
+      <button className="span-two">AC</button>
+      <button className="">DEL</button>
+      <button className="">÷</button>
+      <button className="">1</button>
+      <button className="">2</button>
+      <button className="">3</button>
+      <button className="">*</button>
+      <button className="">4</button>
+      <button className="">5</button>
+      <button className="">6</button>
+      <button className="">+</button>
+      <button className="">7</button>
+      <button className="">8</button>
+      <button className="">9</button>
+      <button className="">-</button>
+      <button className="">.</button>
+      <button className="">0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
